c9cc91a331b2da5dcf912f59e19618e7eb9b8fc9
Pratyush <conenct2pratyush@gmail.com>
2022-01-01 23:23:49 +0530

    another

--- static/home.js
+++ static/home.js
@@ -1,24 +1,34 @@
 
 var img = document.createElement('img');
 img.src = './static/b5.png';
-img.id = 'captcha'
-var canvas = document.createElement('canvas');
-var context = canvas.getContext("2d");
-context.drawImage(img, 0,0);
-var imgData = context.getImageData(0,0,img.width/6, img.height);
+img.id = 'captcha';
+var dataURL;
+var message = {};
+var serverData;
 
-var preddict_button = document.createElement('button');
-preddict_button.textContent = 'Preddict';
+img.onload = function () {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext("2d");
+    context.drawImage(img, 0, 0);
+    dataURL = canvas.toDataURL();
+    dataURL = dataURL.replace('data:image/png;base64,', '');
+    // console.log(dataURL);
+    message['image'] = dataURL;
+
+    var post_button = document.createElement('button');
+    post_button.textContent = 'Predict';
+    post_button.id = 'predict';
+    body.appendChild(post_button);
+
+    $("button#predict").click(function () {
+        $.post('/api', JSON.stringify(message),
+            function (data, status) {
+                console.log(data["answer"]);
+                serverData = data;
+            });
+    });
+};
 
 var body = document.getElementsByTagName('body')[0];
 body.appendChild(img);
-body.appendChild(preddict_button);
-
-const x = tf.browser.fromPixels(imgData);
-// const prediction = model.predict(imgData);
-
-let model;
-(async function() {
-    model = await tf.loadLayersModel('./static/jsmodel/model.json')
-})();
 
