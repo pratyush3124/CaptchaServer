c45da19188e0e931d62b68c1dd706f7c35e1696a
Pratyush <conenct2pratyush@gmail.com>
2022-01-10 15:02:19 +0530

    v2

--- Site/__init__.py
+++ Site/__init__.py
@@ -3,8 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 import razorpay
 from Site import utility
 
+ENV = 'prod'
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1ram@localhost/captcha'
+
+if ENV == 'dev':
+    app.config['DEBUG'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1ram@localhost/captcha'
+else:
+    app.config['DEBUG'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bpubqeyqhekidv:b33f622dd2ada7f376b44b20a9d025db7f264b3590021df8d50b54ef0f263c6b@ec2-52-44-80-40.compute-1.amazonaws.com:5432/d76hkeledml0ld'
+
 db = SQLAlchemy(app)
 
 api_key = "rzp_test_CdqGx08LQ19FSF"
